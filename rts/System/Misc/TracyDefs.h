930a96df17f90ef24087da969c40b5f2f0cd80a1
Daniel Harvey <keithdanielharvey@gmail.com>
2026-01-07 20:47:15 -0700

    wip: fix tracy scoping issue

--- rts/System/Misc/TracyDefs.h
+++ rts/System/Misc/TracyDefs.h
@@ -3,7 +3,8 @@
 #include <tracy/Tracy.hpp>
 
 #ifdef RECOIL_DETAILED_TRACY_ZONING
-	#define RECOIL_DETAILED_TRACY_ZONE ZoneScoped
+	// Use ZoneNamed with a distinct variable name to avoid collision with SCOPED_TIMER's ZoneScopedNC
+	#define RECOIL_DETAILED_TRACY_ZONE ZoneNamed(___tracy_detailed_zone, true)
 #else
 	#define RECOIL_DETAILED_TRACY_ZONE do {} while(0)
 #endif
